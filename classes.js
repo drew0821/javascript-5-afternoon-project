c04918860b1579edd89dbb102f620b1a7fc9d126
drew0821 <tshipp909@gmail.com>
2020-02-24 17:11:10 -0700

    first commit

--- classes.js
+++ classes.js
@@ -29,7 +29,18 @@
   Call your class Employee and receive all the data in the constructor in the order listed above.
 */
 
-//Code Here
+class Employee {
+  constructor(first_name, last_name, email, age){
+  this.first_name = first_name;
+  this.last_name = last_name;
+  this.email = email;
+  this.age = age;
+}
+makeWidget(){
+  return `${this.first_name} ${this.last_name} Widget`
+}
+}
+
 
 
 ////////// PROBLEM 2 //////////
@@ -46,8 +57,23 @@
 
   Call your new class Manager
 */
+class Manager {
+  constructor(first_name, last_name, email, age,){
+  this.first_name = first_name;
+  this.last_name = last_name;
+  this.email = email;
+  this.age = age;
+  this.reports = [];
+} 
+hire(employee){
+  this.reports.push(employee)
 
-//Code Here
+}
+fire(index){
+ this.reports.splice(index,1)
+}
+
+}
 
 
 ////////// PROBLEM 3 //////////
@@ -71,8 +97,43 @@
   Call your new class ProgressiveManager
 */
 
-//Code Here
+class ProgressiveManager {
+  constructor(first_name, last_name, email, age){
+  this.first_name = first_name;
+  this.last_name = last_name;
+  this.email = email;
+  this.age = age;
+  this.reports = [];
+  this.title = 'Not a manager';
+  this.bonus = 0;
+}
+managerTitle (){
+  if (this.reports.length === 0){
+    return this.title = 'Not a manager'
+  } else if (this.reports.length <= 3) {
+    return this.title = 'Barely Manager'
+  } else if (this.reports.length <= 10){
+    return this.title = 'Mostly Manager'
+  } else if( this.reports.length <= 50) {
+    return this.title = 'Manager'
+  } else if (this.reports.length <= 100){
+    return this.title = 'Manager Plus'
+  } else {
+    return this.title = 'Bestest Manager'
+  }
+} 
 
+fire(index){
+ this.bonus += 100
+ this.reports.splice(index,1)
+ this.managerTitle()
+}
+hire(employee){
+  this.reports.push(employee)
+  this.managerTitle()
+}
+
+}
 
 
 ////////// PROBLEM 4 - Black Diamond //////////
--- closures.js
+++ closures.js
@@ -22,13 +22,12 @@ function outer() {
   Invoke outer saving the return value into another variable called 'inner'.
 */
   
-// Code Here
-
+let inner = outer()
 
 
 //Once you do that, invoke inner.
 
-//Code Here
+inner ()
 
 
 
@@ -51,7 +50,9 @@ function callFriend(name) {
   (HINT: You will need to pass in arguments to both function invocations)
 */
 
-//Code Here
+let callJake = callFriend('Jake')
+  callJake(435-555-9248)
+
 
 
 
@@ -61,16 +62,22 @@ function callFriend(name) {
   Write a function called makeCounter that makes the following code work properly.
 */
 
-//Code Here
+function makeCounter(){
+  let count = 0;
+  function addOne(){
+    return count += 1;
+  }
+  return addOne
+}
 
 
 
 //Uncomment this once you make your function
-//   var count = makeCounter();
-//   count(); // 1
-//   count(); // 2
-//   count(); // 3
-//   count(); // 4
+  var count = makeCounter();
+  count(); // 1
+  count(); // 2
+  count(); // 3
+  count(); // 4
 
 
 
@@ -86,9 +93,15 @@ function callFriend(name) {
 */
 
 function counterFactory(value) {
-  // Code here.
+  let number = value;
 
   return {
+    inc(){
+      return number += 1
+    },
+    dec(){
+      return number -= 1
+    }
 
   };
 }
@@ -111,11 +124,13 @@ counter = counterFactory(10);
 
 function motivation( firstname, lastname ) {
   var welcomeText = "You're doing awesome, keep it up";
-
-  // code message function here.
+function message(){
+  return `${welcomeText} ${firstname} ${lastname}.`
+}
+  
 
   //Uncommment this to return the value of your message function
-  //return message;
+  return message;
 }
 
 var greeting = motivation('Billy', 'Bob'); // 'You're doing awesome keep it up Billy Bob.
@@ -143,8 +158,9 @@ var module = (function() {
   // Anything that is being returned is made public and can be invoked from
   // outside our lexical scope
   return {
-    // Code here.
-  };
+    publicMethod() {return privateMethod()}
+    
+  }; module.publicMethod() 
 })();
 
 
@@ -162,7 +178,14 @@ function secretNumber() {
   var secret = 143;
 
   return {
-    // Code here
+    addToSecret(num){
+      secret += num;
+      return secret
+    },
+    takeAwayFromSecret(num){
+      secret -= num;
+      return secret
+    }
   };
 }
 
@@ -188,9 +211,13 @@ function secretNumber() {
 
 function timeOutCounter() {
   for (var i = 0; i <= 5; i++) {
+    closure(i);
+  }
+    function closure(i){
     setTimeout(function() {
       console.log(i);
     }, i * 1000);
   }
-}
+  }
+
 timeOutCounter();
